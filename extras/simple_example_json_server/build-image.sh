6684f3cfdd597d66c5aeec70a94ef8d968c6caae
Partho Bhowmick <p.s.bhowmick@gmail.com>
2024-08-25 15:29:55 -0500

    Redesign helm & kustomize integrations Fixed image build and push

--- extras/simple_example_json_server/build-image.sh
+++ extras/simple_example_json_server/build-image.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
 IMAGE_NAME=python-jsonserver
 REMOTE_IMG_TAG=docker.io/pbhowmic/python-jsonserver:0.1
@@ -6,5 +7,5 @@ REMOTE_IMG_TAG=docker.io/pbhowmic/python-jsonserver:0.1
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 docker build "${SCRIPT_DIR}" -t ${IMAGE_NAME}
 docker tag ${IMAGE_NAME} ${REMOTE_IMG_TAG}
-docker push REMOTE_IMG_TAG
+docker push ${REMOTE_IMG_TAG}
 #kind load docker-image  ${IMAGE_NAME} -n deploydocus
